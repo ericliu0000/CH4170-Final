f34400c00f3533cc33ff76738ef169af7043fb0c
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-12-04 14:39:51 -0500

    Add info to redox text
    Add battery cell potential stuff

--- redox.py
+++ redox.py
@@ -29,7 +29,8 @@ class Redox(Scene):
         e2 = e1.copy().move_to((-2, -0.7, 0))
         s = Circle(0.4, YELLOW_A, fill_opacity=1).move_to((3, 0, 0))
 
-        li2s_text = MarkupText("Li<sub>2</sub>S", font_size=36, font="IBM Plex Mono").shift(DOWN)
+        li2s_text1 = MarkupText("Li<sub>2</sub>S", font_size=36, font="IBM Plex Mono").shift(DOWN * 1)
+        li2s_text2 = MarkupText("E<sup>o</sup>cell ~= 2.20 V", font_size=24, font="IBM Plex Mono").next_to(li2s_text1, DOWN, buff=0.2)
         li2s_group = Group(li1, li2, s)
 
         self.play(FadeIn(li1, li2, e1, e2, s))
@@ -41,18 +42,17 @@ class Redox(Scene):
                                    e2.animate().shift(RIGHT * 6),
                                    s.animate().shift(LEFT * 2)],
                             run_time=constants.ANIMATION_TIME)
-        constants.bulk_play(self, [li2s_group.animate().rotate(PI / 2),
-                                   FadeOut(e1, e2)], run_time=constants.ANIMATION_TIME)
+        constants.bulk_play(self, [li2s_group.animate().rotate(PI / 2).move_to((0, 0, 0)),
+                                   FadeOut(e1, e2)], run_time=constants.LONG_ANIMATION_TIME)
 
         # Add text
-        self.play(li2s_group.animate().move_to((0, 0, 0)))
-        self.play(Create(li2s_text))
+        constants.bulk_play(self, Create(li2s_text1), Create(li2s_text2))
         self.wait(constants.LONG_DWELL_TIME)
 
         # Explode everything
         left = Group(cell.anode, cell.anode_text)
         right = Group(cell.cathode, cell.cathode_text)
-        down = Group(li2s_group, li2s_text)
+        down = Group(li2s_group, li2s_text1, li2s_text2)
         constants.bulk_play(self, [left.animate().shift(LEFT * 8),
                                    right.animate().shift(RIGHT * 8),
                                    cell.electrolyte.animate().shift(UP * 8),
