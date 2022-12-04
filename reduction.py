fbe56c1f00b5d79872bce80b8d65fd7d20b81ac8
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-12-04 13:56:08 -0500

    Fix how reduction works
    oops... I copied the oxidation animations and forgot that
    reduction is gaining electrons, not losing

--- reduction.py
+++ reduction.py
@@ -25,7 +25,7 @@ class Reduction(Scene):
         electrode_text = MarkupText("C/S", font_size=24, font="IBM Plex Mono", color=BLACK).move_to(electrode.get_center()).shift(UP)
         electrode_group = Group(electrode, electrode_text)
 
-        s = Circle(0.5, GRAY_C, fill_opacity=1)
+        s = Circle(0.5, YELLOW_A, fill_opacity=1)
         s_text = MarkupText("S", font_size=24, font="IBM Plex Mono", color=BLACK).move_to(s.get_center())
         s_text2 = MarkupText("S<sup>2-</sup>", font_size=24, font="IBM Plex Mono", color=BLACK).move_to(s.get_center())
         s_group = Group(s, s_text, z_index=1).scale(0.001)
@@ -34,6 +34,7 @@ class Reduction(Scene):
         e_text = MarkupText("e<sup>-</sup>", font_size=16, font="IBM Plex Mono", color=BLACK).move_to(e.get_center())
         e_group = Group(e, e_text)
         e_group2 = e_group.copy()
+        e_group, e_group2 = e_group.shift(UP + LEFT), e_group2.shift(UP + RIGHT)
 
         red_rxn = MarkupText("S + 2e<sup>-</sup> → S<sup>2-</sup>", font_size=36, font="IBM Plex Mono", color=WHITE).shift(DOWN * 2)
 
@@ -44,7 +45,8 @@ class Reduction(Scene):
 
         # Zoom in both
         constants.bulk_play(self, [electrode_group.animate().scale(14.2),
-                                   s_group.animate().scale(1000)],
+                                   s_group.animate().scale(1000),
+                                   FadeIn(e_group, e_group2)],
                             run_time=constants.LONG_ANIMATION_TIME)
         self.play(electrode_group.animate(run_time=constants.ANIMATION_TIME,
                                       rate_func=rate_functions.ease_in_sine)
@@ -52,17 +54,18 @@ class Reduction(Scene):
 
         # Create electron, seperate from Li
         self.add(e, e_text)
-        constants.bulk_play(self, [e_group.animate(run_time=constants.SHORT_ANIMATION_TIME).shift(UP + LEFT), 
-                                    e_group2.animate(run_time=constants.SHORT_ANIMATION_TIME).shift(UP + RIGHT),
+        constants.bulk_play(self, [e_group.animate(run_time=constants.SHORT_ANIMATION_TIME).shift(DOWN + RIGHT), 
+                                    e_group2.animate(run_time=constants.SHORT_ANIMATION_TIME).shift(DOWN + LEFT),
                                    ReplacementTransform(s_text, s_text2),
                                    electrode_group.animate(rate_func=rate_functions.ease_out_sine).shift(RIGHT * 10)],
                             run_time=constants.LONG_ANIMATION_TIME)
-
+        
+        self.remove(e_group, e_group2)
         self.play(Create(red_rxn))
         self.wait(constants.LONG_DWELL_TIME)
 
         # Group everything and move it away, bring back the cell
-        all_group = Group(s_group, e_group, e_group2, red_rxn)
+        all_group = Group(s_group, red_rxn)
         conclusion_cell = Cell().cell_group.shift(LEFT * 15)
         constants.bulk_play(self, [all_group.animate().shift(RIGHT * 15),
                                       conclusion_cell.animate().shift(RIGHT * 15)],
